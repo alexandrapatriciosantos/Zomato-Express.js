c30c9fd79e5fa5d0f7d6b2b097b1a69bc0e18751
Alexandra Santos <alexandraps17@gmail.com>
2020-01-07 15:38:19 +0000

    db crete

--- db/create.js
+++ db/create.js
@@ -1,242 +1,196 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`UserType`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`UserType` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_type` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+const connection = require('./config'); 
 
 
--- -----------------------------------------------------
--- Table `mydb`.`Region`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Region` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+const UserType = `
+  CREATE TABLE IF NOT EXISTS user_type (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_type INT NOT NULL,
+    PRIMARY KEY (id)
+  );
+`
+
+const Region = `
+  CREATE TABLE IF NOT EXISTS region (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+  );
+`
+const User = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(225) NOT NULL,
+    last_name VARCHAR(225) NOT NULL,
+    email VARCHAR(225) NOT NULL,
+    phone_number VARCHAR(225) NULL,
+    user_type_id INT NOT NULL,
+    region_id INT NOT NULL,
+    PRIMARY KEY (id, region_id),
+      FOREIGN KEY (user_type_id)
+      REFERENCES mydb.user_type (id),
+      FOREIGN KEY (region_id)
+      REFERENCES mydb.region (id)
+    );
+`
+const Result = `
+  CREATE TABLE IF NOT EXISTS result (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    time_to_complete TIME, 
+    time_of_day DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id)
+    REFERENCES mydb.user (id)
+  );
+`
+const Restaurant = `
+  CREATE TABLE IF NOT EXISTS restaurant (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    region_id INT NOT NULL,
+    PRIMARY KEY (id, region_id),
+    FOREIGN KEY (region_id)
+    REFERENCES mydb.region (id)
+  );
+`
+const Language = `
+  CREATE TABLE IF NOT EXISTS language (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+  );
+`
+const Question = `
+  CREATE TABLE IF NOT EXISTS question (
+    id INT NOT NULL AUTO_INCREMENT,
+    question VARCHAR(255) NOT NULL,
+    correct_answer_id INT NOT NULL,
+    language_id INT NOT NULL,
+    PRIMARY KEY (id, language_id),
+    FOREIGN KEY (language_id)
+    REFERENCES mydb.language (id)
+  );
+`
+const QuestionResult = `
+  CREATE TABLE IF NOT EXISTS question_result (
+    result_id INT NOT NULL,
+    question_id INT NOT NULL,
+    question_language_id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    user_answer_id INT NOT NULL,
+    PRIMARY KEY (id, result_id, question_id, question_language_id),
+    FOREIGN KEY (result_id)
+    REFERENCES mydb.result (id),
+    FOREIGN KEY (question_id , question_language_id)
+    REFERENCES mydb.question (id , language_id)
+  );
+`
+const UserRestaurant = `
+  CREATE TABLE IF NOT EXISTS user_restaurant (
+    restaurant_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (restaurant_id, user_id),
+    FOREIGN KEY (restaurant_id)
+    REFERENCES mydb.restaurant (id),
+    FOREIGN KEY (user_id)
+    REFERENCES mydb.user (id)
+  );
+`
+
+const Answer = `
+  CREATE TABLE IF NOT EXISTS answer (
+    id INT NOT NULL AUTO_INCREMENT,
+    answer_option VARCHAR(255) NOT NULL,
+    question_id INT NOT NULL,
+    question_language_id INT NOT NULL,
+    PRIMARY KEY (id, question_id, question_language_id),
+    FOREIGN KEY (question_id , question_language_id)
+    REFERENCES mydb.question (id , language_id)
+  );
+`
 
 
--- -----------------------------------------------------
--- Table `mydb`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`User` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(225) NOT NULL,
-  `last_name` VARCHAR(225) NOT NULL,
-  `email` VARCHAR(225) NOT NULL,
-  `phone_number` VARCHAR(225) NULL,
-  `UserType_id` INT NOT NULL,
-  `Region_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `Region_id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_User_UserType_idx` (`UserType_id` ASC),
-  INDEX `fk_User_Region1_idx` (`Region_id` ASC),
-  CONSTRAINT `fk_User_UserType`
-    FOREIGN KEY (`UserType_id`)
-    REFERENCES `mydb`.`UserType` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_User_Region1`
-    FOREIGN KEY (`Region_id`)
-    REFERENCES `mydb`.`Region` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Result`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Result` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `time_to_complete` DATETIME GENERATED ALWAYS AS (),
-  `User_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_Result_User1_idx` (`User_id` ASC),
-  CONSTRAINT `fk_Result_User1`
-    FOREIGN KEY (`User_id`)
-    REFERENCES `mydb`.`User` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Restaurant`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Restaurant` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `User_id` INT NOT NULL,
-  `Region_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `Region_id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_Restaurant_Region1_idx` (`Region_id` ASC),
-  CONSTRAINT `fk_Restaurant_Region1`
-    FOREIGN KEY (`Region_id`)
-    REFERENCES `mydb`.`Region` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Language`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Language` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Question`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Question` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `question` VARCHAR(255) NOT NULL,
-  `correct_answer_id` INT NOT NULL,
-  `Language_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `Language_id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_Question_Language1_idx` (`Language_id` ASC),
-  CONSTRAINT `fk_Question_Language1`
-    FOREIGN KEY (`Language_id`)
-    REFERENCES `mydb`.`Language` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`QuestionResult`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`QuestionResult` (
-  `Result_id` INT UNSIGNED NOT NULL,
-  `Question_id` INT UNSIGNED NOT NULL,
-  `Question_Language_id` INT UNSIGNED NOT NULL,
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_answer_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Result_id`, `Question_id`, `Question_Language_id`),
-  INDEX `fk_Result_has_Question_Question1_idx` (`Question_id` ASC, `Question_Language_id` ASC),
-  INDEX `fk_Result_has_Question_Result1_idx` (`Result_id` ASC),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  CONSTRAINT `fk_Result_has_Question_Result1`
-    FOREIGN KEY (`Result_id`)
-    REFERENCES `mydb`.`Result` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Result_has_Question_Question1`
-    FOREIGN KEY (`Question_id` , `Question_Language_id`)
-    REFERENCES `mydb`.`Question` (`id` , `Language_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`UserRestaurant`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`UserRestaurant` (
-  `Restaurant_id` INT UNSIGNED NOT NULL,
-  `User_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`Restaurant_id`, `User_id`),
-  INDEX `fk_Restaurant_has_User_User1_idx` (`User_id` ASC),
-  INDEX `fk_Restaurant_has_User_Restaurant1_idx` (`Restaurant_id` ASC),
-  CONSTRAINT `fk_Restaurant_has_User_Restaurant1`
-    FOREIGN KEY (`Restaurant_id`)
-    REFERENCES `mydb`.`Restaurant` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Restaurant_has_User_User1`
-    FOREIGN KEY (`User_id`)
-    REFERENCES `mydb`.`User` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Answer`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Answer` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `answer_option` VARCHAR(255) NOT NULL,
-  `Question_id` INT UNSIGNED NOT NULL,
-  `Question_Language_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `Question_id`, `Question_Language_id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_Answer_Question1_idx` (`Question_id` ASC, `Question_Language_id` ASC),
-  CONSTRAINT `fk_Answer_Question1`
-    FOREIGN KEY (`Question_id` , `Question_Language_id`)
-    REFERENCES `mydb`.`Question` (`id` , `Language_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`QuestionResult`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`QuestionResult` (
-  `Result_id` INT UNSIGNED NOT NULL,
-  `Question_id` INT UNSIGNED NOT NULL,
-  `Question_Language_id` INT UNSIGNED NOT NULL,
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_answer_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Result_id`, `Question_id`, `Question_Language_id`),
-  INDEX `fk_Result_has_Question_Question1_idx` (`Question_id` ASC, `Question_Language_id` ASC),
-  INDEX `fk_Result_has_Question_Result1_idx` (`Result_id` ASC),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  CONSTRAINT `fk_Result_has_Question_Result1`
-    FOREIGN KEY (`Result_id`)
-    REFERENCES `mydb`.`Result` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Result_has_Question_Question1`
-    FOREIGN KEY (`Question_id` , `Question_Language_id`)
-    REFERENCES `mydb`.`Question` (`id` , `Language_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Placeholder table for view `mydb`.`view1`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`view1` (`id` INT);
-
--- -----------------------------------------------------
--- View `mydb`.`view1`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`view1`;
-USE `mydb`;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+connection.query(UserType, (err) =>{
+  if (err) {
+    console.log(err)
+    connection.end();
+  } else {
+    console.log("user_type created")
+    connection.query(Region, (err) =>{
+      if (err) {
+        console.log(err)
+        connection.end();
+      } else {
+        console.log("region created")
+        connection.query(User, (err) =>{
+          if (err) {
+            console.log(err)
+            connection.end();
+          } else {
+            console.log("user created")
+            connection.query(Result, (err) =>{
+              if (err) {
+                console.log(err)
+                connection.end();
+              } else {
+                console.log("result created")
+                connection.query(Restaurant, (err) =>{
+                  if (err) {
+                    console.log(err)
+                    connection.end();
+                  } else {
+                    console.log("restaurant created")
+                    connection.query(Language, (err) =>{
+                      if (err) {
+                        console.log(err)
+                        connection.end();
+                      } else {
+                        console.log("language created")
+                        connection.query(Question, (err) =>{
+                          if (err) {
+                            console.log(err)
+                            connection.end();
+                          } else {
+                            console.log("question created")
+                            connection.query(QuestionResult, (err) =>{
+                              if (err) {
+                                console.log(err)
+                                connection.end();
+                              } else {
+                                console.log("question_result created")
+                                connection.query(UserRestaurant, (err) =>{
+                                  if (err) {
+                                    console.log(err)
+                                    connection.end();
+                                  } else {
+                                    console.log("use_restaurant created")
+                                    connection.query(Answer, (err) =>{
+                                      if (err) {
+                                        console.log(err)
+                                        connection.end();
+                                      } else {
+                                        console.log("answer created")
+                                        connection.end();
+                                      }
+                                    });
+                                  }
+                                }); 
+                              }
+                            }); 
+                          }
+                        }); 
+                      }
+                    }); 
+                  }
+                }); 
+              }
+            }); 
+          }
+        }); 
+      }
+    }); 
+  }
+  
+}); 
