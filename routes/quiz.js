fcd3a62190c24dabb514836c0cfc285ad1a7195b
edgwatson1 <e@edwardwatson.com>
2020-01-23 16:53:07 +0000

    add_learner_get_all_quizzes

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const { authenticateWithJwt } = require('../services/jwt');
+const express = require("express");
+const { authenticateWithJwt } = require("../services/jwt");
+const { getAllQuizzes } = require("../controllers/admin/quiz-controller");
 
 const router = express.Router();
 
-router.get('/', authenticateWithJwt, (req, res) => res.json({ hey: 'hey' }));
+router.get("/", authenticateWithJwt, getAllQuizzes);
 
-router.get('/:id/question/:questionid', (req, res) => { // how to make sure this is a sub of quiz?? 
-  res.send('quiz/:id - on quiz');
+router.get("/:id/question/:questionid", (req, res) => {
+  // how to make sure this is a sub of quiz??
+  res.send("quiz/:id - on quiz");
 });
 
-router.get('/results', (req, res) => {
-  res.send('quiz/results');
+router.get("/results", (req, res) => {
+  res.send("quiz/results");
 });
 
 module.exports = router;
