dd235eba2deba2f2f513def1f97f37c1957838cb
Angélina RIET <angelina.riet@yahoo.com>
2020-01-31 11:21:26 +0000

    lang working

--- controllers/language-controller.js
+++ controllers/language-controller.js
@@ -1,16 +1,19 @@
 
 const getBrowserLang = (req, res, next) => {
   const preferredLanguage = req.get('Preferred-Language');
-  console.log(preferredLanguage);
 
   let languageId = 1;
+  let language = 'en';
   if (preferredLanguage === 'pt') {
     languageId = 2;
+    language = 'pt';
   }
   if (!preferredLanguage && req.acceptsLanguages('pt')) {
     languageId = 2;
+    language = 'pt';
   }
   req.languageId = languageId;
+  req.language = language;
   next();
 };
 
---
+++ i18n/translations.js
@@ -0,0 +1,18 @@
+const Translations = {
+  en: {
+    Flash: {
+      userHasBeenSignedUp: 'User has been signed up!',
+      emailAlreadyExists: 'Email already exists',
+      EmailOrPasswordIsIncorrect: 'Email or password is incorrect',
+    },
+  },
+  pt: {
+    Flash: {
+      userHasBeenSignedUp: 'User has been signed up! (portuguese please)',
+      emailAlreadyExists: 'Email already exists (portuguese please)',
+      EmailOrPasswordIsIncorrect: 'Email or password is incorrect (portuguese please)',
+    },
+  },
+};
+
+module.exports = Translations;
