9a2cfd13c3519ef3938fa50018dbec2e2c157835
Alexandra Santos <alexandraps17@gmail.com>
2020-02-04 11:09:47 +0000

    dotenv

--- db/config.js
+++ db/config.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
-const keys = require('../keys');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: keys.MYSQL_USER,
-  password: keys.MYSQL_PASSWORD,
-  database: keys.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
