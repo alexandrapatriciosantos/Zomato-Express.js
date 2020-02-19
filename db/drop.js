b1e5d3ae32ff647119f9b14a7709a1655b8160ae
Angélina RIET <angelina.riet@yahoo.com>
2020-02-19 18:52:29 +0000

    table contact removed and faq seeded again

--- db/drop.js
+++ db/drop.js
@@ -4,44 +4,40 @@ const connection = require('./config');
 connection.query('DROP TABLE faq', (err) => {
   if (err) console.log(err);
   console.log('faq deleted');
-  connection.query('DROP TABLE contact', (err) => {
+  connection.query('DROP TABLE documentation', (err) => {
     if (err) console.log(err);
-    console.log('contact deleted');
-    connection.query('DROP TABLE documentation', (err) => {
+    console.log('documentation deleted');
+    connection.query('DROP TABLE result', (err) => {
       if (err) console.log(err);
-      console.log('documentation deleted');
-      connection.query('DROP TABLE result', (err) => {
+      console.log('result deleted');
+      connection.query('DROP TABLE answer', (err) => {
         if (err) console.log(err);
-        console.log('result deleted');
-        connection.query('DROP TABLE answer', (err) => {
+        console.log('answer deleted');
+        connection.query('DROP TABLE question', (err) => {
           if (err) console.log(err);
-          console.log('answer deleted');
-          connection.query('DROP TABLE question', (err) => {
+          console.log('question deleted');
+          connection.query('DROP TABLE quiz', (err) => {
             if (err) console.log(err);
-            console.log('question deleted');
-            connection.query('DROP TABLE quiz', (err) => {
+            console.log('quiz deleted');
+            connection.query('DROP TABLE product', (err) => {
               if (err) console.log(err);
-              console.log('quiz deleted');
-              connection.query('DROP TABLE product', (err) => {
+              console.log('product deleted');
+              connection.query('DROP TABLE language', (err) => {
                 if (err) console.log(err);
-                console.log('product deleted');
-                connection.query('DROP TABLE language', (err) => {
+                console.log('language deleted');
+                connection.query('DROP TABLE user', (err) => {
                   if (err) console.log(err);
-                  console.log('language deleted');
-                  connection.query('DROP TABLE user', (err) => {
+                  console.log('user deleted');
+                  connection.query('DROP TABLE restaurant', (err) => {
                     if (err) console.log(err);
-                    console.log('user deleted');
-                    connection.query('DROP TABLE restaurant', (err) => {
+                    console.log('restaurant deleted');
+                    connection.query('DROP TABLE region', (err) => {
                       if (err) console.log(err);
-                      console.log('restaurant deleted');
-                      connection.query('DROP TABLE region', (err) => {
+                      console.log('region deleted');
+                      connection.query('DROP TABLE user_type', (err) => {
                         if (err) console.log(err);
-                        console.log('region deleted');
-                        connection.query('DROP TABLE user_type', (err) => {
-                          if (err) console.log(err);
-                          console.log('user_type deleted');
-                          connection.end();
-                        });
+                        console.log('user_type deleted');
+                        connection.end();
                       });
                     });
                   });
